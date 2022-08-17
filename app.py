ee6dd7c6e27a3e3a288e44dbc36f4bd1e48e180c
zaki1003 <mz.merabet@esi-sba.dz>
2022-08-17 19:07:24 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+DATA_URL = ()
+
+st.title("Motor Vehicule Collisions in New York City")
+st.markdown("This application is a Streamlit dashboard that can be used to analyse motor vehicule collisions in NYC")
